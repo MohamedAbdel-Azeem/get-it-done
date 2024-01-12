2fc1ce16842a28a779ff29f1fc9113dab44eb7db
Mohamed Abdelazeem <mohamedk.abdelazeem@gmail.com>
2024-01-12 12:40:38 +0200

    Sidebar color chosen and Add Button Added without functionality

--- src/view/index.js
+++ src/view/index.js
@@ -22,8 +22,8 @@ function topBar(){
   const switchModeButton = document.createElement('button');
 
   topBar.id = 'top-bar';
-  topBar.className = 'w-full bg-slate-50 dark:bg-blue-950 flex flex-row space-x-4 items-center justify-center max-md:start';
-  title.className =  'text-5xl text-slate-950 dark:text-slate-50 max-md:text-2xl';
+  topBar.className = 'w-full bg-slate-100 dark:bg-blue-900 flex flex-row space-x-4 items-center justify-center max-md:start';
+  title.className =  'text-5xl text-slate-950 dark:text-slate-50 max-md:text-2xl transition-all duration-500';
   span.className = 'text-6xl text-green-600 dark:text-green-400 max-md:text-3xl';
   title.textContent = 'Get it ';
   span.textContent = 'Done';
@@ -53,19 +53,50 @@ function topBar(){
 }
 
 
+function sidebar(){
+  const sidebar = document.createElement('aside');
+  const projectsHeader = document.createElement('h3');
+  const addProjectButton = document.createElement('button');
+
+  let addProjectIcon = document.createElement('div');
+  addProjectIcon.innerHTML = `
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width=40>
+  <title>plus</title>
+  <path d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z" />
+  </svg>
+  `;
+  
+  sidebar.id = 'sidebar';
+
+  sidebar.classList = 'h-full bg-slate-200 dark:bg-blue-950 flex flex-col items-center';
+  projectsHeader.className = 'text-center pt-4 underline underline-offset-8 text-3xl text-slate-900 dark:text-slate-50 max-md:text-xl transition-all duration-500 hover:text-4xl hover:max-md:text-2xl';
+  projectsHeader.style.fontFamily = 'lowerHeaders';
+  addProjectButton.classList = 'mt-4 bg-slate-300 dark:bg-indigo-950 w-3/4 flex flex-row justify-center items-center rounded-md transition-all duration-500 shadow-md hover:shadow-xl';
+
+  addProjectIcon.classList = 'fill-green-600 dark:fill-green-400'
+
+  projectsHeader.textContent = 'Projects';
+
+  addProjectButton.appendChild(addProjectIcon);
+  sidebar.appendChild(projectsHeader);
+  sidebar.appendChild(addProjectButton);
+  return sidebar;
+}
+
 
 
 function mainTag(){
   const main = document.createElement('main');
-  const sidebar = document.createElement('aside');
+  
   const content = document.createElement('div');
 
-  sidebar.id = 'sidebar';
-  content.id = 'content';
 
+  
+  content.id = 'content';
+  const sidebarElement = sidebar();
     // Append elements
 
-  main.appendChild(sidebar);
+  main.appendChild(sidebarElement);
   main.appendChild(content);
 
   return main;
